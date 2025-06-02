688543ddfb0b0f1d498848055b35a7193762e3bd
Josefus2003 <josefus2003@gmail.com>
2025-06-01 21:51:17 -0400

    404 sin redirect

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,26 +1,14 @@
-"use client";
-
-import { useEffect } from "react";
-
 export default function NotFound() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      window.location.href = "/";
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="min-h-[calc(100vh-16rem)] flex flex-col items-center justify-center">
       <img src="/images/sad.png" alt="404" height={200} width={200} />
       <div className="mt-8 text-center">
-        <p className="text-3xl font-bold mt-4">
+        <p className="text-3xl font-bold mt-4 mb-8">
           Esta página ha cambiado de lugar.
         </p>
-        <p className="text-2xl mt-4 text-slate-600">
-          Un momento por favor...
-        </p>
+        <a href="https://termotronic.com" className="text-2xl mt-8 text-blue-600 underline">
+          Clic aquí para continuar
+        </a>
       </div>
     </div>
   );
